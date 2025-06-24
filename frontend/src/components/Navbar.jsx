b0de646c97e7f273672407f8627ae4fd62828a6d
bari@199 <bari1999smab@gmail.com>
2025-06-24 09:52:11 -0700

    added navlink

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,19 +10,31 @@ const Navbar = () => {
       <img src={assets.logo} className="w-36" alt="logo" />
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-900">
-        <NavLink to="/" className="flex flex-col items-center gap-1">
+        <NavLink
+          to="/"
+          className="py-2 pl-6 border flex flex-col items-center gap-1"
+        >
           <p>HOME</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+        <NavLink
+          to="/collection"
+          className="py-2 pl-6 border flex flex-col items-center gap-1"
+        >
           <p>COLLECTION</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to="/about" className="flex flex-col items-center gap-1">
+        <NavLink
+          to="/about"
+          className="py-2 pl-6 border flex flex-col items-center gap-1"
+        >
           <p>ABOUT</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
-        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+        <NavLink
+          to="/contact"
+          className="py-2 pl-6 border flex flex-col items-center gap-1"
+        >
           <p>CONTACT</p>
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
@@ -49,28 +61,34 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <Link to="/cart" className="relative">
+
+        <Link to="/cart" className="relative py-2 pl-6 border">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart_icon" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             10
           </p>
         </Link>
+
         <img
-        onClick={()=>setVisible(true)}
+          onClick={() => setVisible(true)}
           src={assets.menu_icon}
           className="w-5 cursor-pointer sm:hidden"
           alt="menu_icon"
         />
       </div>
-      {/* SideBar Menu for small screen */}
-      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
-        <div className='flex flex-col text-gray-600'>
-            <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3">
-                <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="drop_down_icon"  />
-                <p>Back</p>
-            </div>
-        </div>
 
+      {/* SideBar Menu for small screen */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}>
+        <div className="flex flex-col text-gray-600">
+          <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3">
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="drop_down_icon" />
+            <p>Back</p>
+          </div>
+          <NavLink className="py-2 pl-6 border" to="/">HOME</NavLink>
+          <NavLink className="py-2 pl-6 border" to="/collection">COLLECTION</NavLink>
+          <NavLink className="py-2 pl-6 border" to="/about">ABOUT</NavLink>
+          <NavLink className="py-2 pl-6 border" to="/contact">CONTACT</NavLink>
+        </div>
       </div>
     </div>
   );
