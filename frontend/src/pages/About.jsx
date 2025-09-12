28b97a4a892f21628bc8de00a46f562c946fe91c
bari@199 <bari1999smab@gmail.com>
2025-09-11 23:00:11 -0700

    added Login or About us pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t ">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            At our core, we believe fashion is more than just clothing—it’s
+            self-expression. We design pieces that blend comfort with timeless
+            style, so you look and feel confident. Every outfit we create tells
+            a story of elegance, passion, and individuality.
+          </p>
+          <p> From everyday wear
+            to statement looks, we make fashion accessible for everyone. Our
+            mission is simple: help you wear confidence, because true style
+            never fades.</p>
+            <b className="text-gray-800">Our Mission</b>
+            <p>Our mission is to empower individuals to express themselves through fashion that is stylish, comfortable, and accessible. We strive to create collections that inspire confidence, celebrate diversity, and bring elegance into everyday life.</p>
+        </div>
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance :</b>
+          <p className="text-gray-600">We believe that true style begins with uncompromised quality. Every product is carefully crafted from premium materials and undergoes strict quality checks to ensure durability, comfort, and elegance.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience :</b>
+          <p className="text-gray-600">Shopping with us is designed to be simple, seamless, and stress-free. From easy navigation and secure checkout to fast delivery and hassle-free returns, we put your comfort first. Our goal is to make fashion accessible at your fingertips—anytime, anywhere.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">Our customers are at the heart of everything we do. We are committed to providing friendly, responsive, and reliable support at every step of your shopping journey.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,39 +1,51 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from '../components/Title';
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-   
-  const { products, currency} = useContext(ShopContext);
-
+  const { products, currency } = useContext(ShopContext);
 
   return (
-    <div className='border-t pt-16'>
-      <div className='text-2xl'>
-        <Title text1={'MY'} text2={'ORDERS'} />
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"MY"} text2={"ORDERS"} />
       </div>
-      <div>
-
-      </div>
-        {
-          products.slice(1,4).map((item,index)=>(
-            <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:justify-between gap-4'>
-                <div className='flex items-start gap-6 text-sm '>
-                    <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
-                    <div>
-                      <p className='sm:text-base font-medium' >{item.name}</p>
-                      <div className='flex items-center gap-3 mt-2 text-base text-gray-700'>
-                        <p className='text-lg'>{currency}{item.price}</p>
-                        <p>{}</p>
-                        <p></p>
-                      </div>
-                    </div>
-                </div>
+      <div></div>
+      {products.slice(1, 4).map((item, index) => (
+        <div
+          key={index}
+          className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:justify-between gap-4"
+        >
+          <div className="flex items-start gap-6 text-sm ">
+            <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
+            <div>
+              <p className="sm:text-base font-medium">{item.name}</p>
+              <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                <p className="text-lg">
+                  {currency}
+                  {item.price}
+                </p>
+                <p>Quantity:1</p>
+                <p>Size:M</p>
+              </div>
+              <p className="mt-2">
+                Date: <span className="text-gray-400">25, Jul, 2025</span>
+              </p>
             </div>
-          ))
-        }
+          </div>
+          <div className="md:w-1/2 flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <p className="w-2 h-2 rounded-full bg-green-500"></p>
+              <p className="text-sm md:text-base">Ready to Ship</p>
+            </div>
+            <button className="border px-3 py-1 text-xs md:text-sm font-medium rounded-sm">
+              Track Order
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
