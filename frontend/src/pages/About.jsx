d067f8008d12e2861969388102113f2f03997bd3
bari@199 <bari1999smab@gmail.com>
2025-09-12 01:35:22 -0700

    Added Contact Us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const About = () => {
   return (
@@ -32,13 +33,15 @@ const About = () => {
       <div className="text-xl py-4">
         <Title text1={'WHY'} text2={'CHOOSE US'}/>
       </div>
+
+
       <div className="flex flex-col md:flex-row text-sm mb-20">
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-          <b>Quality Assurance :</b>
+          <b>Quality Assurance:</b>
           <p className="text-gray-600">We believe that true style begins with uncompromised quality. Every product is carefully crafted from premium materials and undergoes strict quality checks to ensure durability, comfort, and elegance.</p>
         </div>
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
-          <b>Convenience :</b>
+          <b>Convenience:</b>
           <p className="text-gray-600">Shopping with us is designed to be simple, seamless, and stress-free. From easy navigation and secure checkout to fast delivery and hassle-free returns, we put your comfort first. Our goal is to make fashion accessible at your fingertips—anytime, anywhere.</p>
         </div>
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
@@ -46,6 +49,8 @@ const About = () => {
           <p className="text-gray-600">Our customers are at the heart of everything we do. We are committed to providing friendly, responsive, and reliable support at every step of your shopping journey.</p>
         </div>
       </div>
+
+      <NewsletterBox/>
     </div>
   );
 };
