84ae928a0ede150c2bc23a470f97033c7c5fa795
bari@199 <bari1999smab@gmail.com>
2025-09-19 11:29:36 -0700

    added payment Intigration

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,6 +6,7 @@ import {
   allOrders,
   userOrders,
   updateStatus,
+  verifyStripe,
 } from "../controllers/orderController.js";
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from "../middleware/auth.js";
@@ -14,7 +15,7 @@ import authUser from "../middleware/auth.js";
 const orderRouter = express.Router()
 
 //Admin Features 
-orderRouter.post('list',adminAuth,allOrders)
+orderRouter.post('/list',adminAuth,allOrders)
 orderRouter.post('/status',adminAuth,updateStatus)
 
 //Payment Features
@@ -27,5 +28,7 @@ orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 
 orderRouter.post('/userorders',authUser,userOrders)
 
+//verify payment
+orderRouter.post('/verifyStripe',authUser,verifyStripe)
 
 export default orderRouter
